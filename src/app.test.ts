c29a251e2d86ca87c9021e28094c5f2556483819
DryDish <57399961+DryDish@users.noreply.github.com>
2022-05-17 10:23:08 +0200

    make test fail

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,6 @@
 // Dummy test to validate CI flow
 describe("Calculator tests", () => {
   test("adding 1 + 2 should return 3", () => {
-    expect((1 + 2)).toBe(3);
+    expect((1 + 1)).toBe(3);
   });
 });
