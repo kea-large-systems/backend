cf64a7a61723d38a13259f31bd465c9d536dade6
Astthor <a.bragason@gmail.com>
2022-06-06 13:52:35 +0200

    Subject router testing db sync fix

--- test/routes/subject-router.test.ts
+++ test/routes/subject-router.test.ts
@@ -7,45 +7,47 @@ import passport from "passport";
 import { Sequelize } from "sequelize";
 import { loadDB } from "../../src/utils/model-loader";
 import { Express } from "express-serve-static-core";
+import "dotenv/config"
 
 let app: Express;
 let sequelize: Sequelize;
-beforeAll(async () => {
-  sequelize = new Sequelize("large_systems_mandatory", "alex", "45061", {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
-    define: {
-      timestamps: false,
-    },
-  });
-  await loadDB(sequelize);
-  subjectAssociationInit()
-  app = express();
-  app.use(json());
-  app.use(passport.initialize());
-  app.use((req, res, next) => {
-    req.isAuthenticated = () => true;
-    req.user = {
-      email: "abcd@test.com",
-      name: "big fart",
-      roleId: TEACHER_ROLE_ID,
-      userId: "1",
-    };
-    next();
-  });
-  app.use(SubjectRouter);
-  subjectInit(sequelize);
-});
-
+const {DB_USERNAME,DB_PASSWORD,DB_HOST,DATABASE} = process.env;
 describe("test subject router", () => {
+
+	beforeAll(async () => {
+		sequelize = new Sequelize(DATABASE!, DB_USERNAME!, DB_PASSWORD!, {
+      host: DB_HOST!,
+      dialect: "mysql",
+      logging: false,
+      define: {
+        timestamps: false,
+      },
+    });
+
+		await loadDB(sequelize);
+
+		app = express();
+		app.use(json());
+		app.use(passport.initialize());
+		app.use((req, res, next) => {
+			req.isAuthenticated = () => true;
+			req.user = {
+				email: "abcd@test.com",
+				name: "big fart",
+				roleId: TEACHER_ROLE_ID,
+				userId: "1",
+			};
+			next();
+		});
+		app.use(SubjectRouter);
+	});
 
   describe("/:subjectId", () => {
     test("Id that exists in database", async () => {
       const response = await request(app)
         .get("/1");
       expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual({ name: "Testing SW20", subjectId: 1 });
+      expect(response.body).toStrictEqual({ name: "Testing SW20", subjectId: 1, classId: 1, teacherUserId: 1 });
     });
     test("ID that does not exist in the database", async () => {
       const response = await request(app)
@@ -74,8 +76,14 @@ describe("test subject router", () => {
     test("checks create new subject", async () => {
       const response = await request(app)
         .post("/").send({ name: "test post", classId: "1",  teacherUserId: "1" });
-      console.log(response.body);
-      console.log(response.status);
+			expect(response.status).toBe(201);
+			expect(response.body).toStrictEqual({ subjectId: 13, name: 'test post', teacherUserId: '1', classId: '1' });
+    });
+		test("checks create new subject", async () => {
+      const response = await request(app)
+        .post("/").send({ name: "test post", classId: "1",  teacherUserId: "1" });
+			expect(response.status).toBe(201);
+			expect(response.body).toStrictEqual({ subjectId: 13, name: 'test post', teacherUserId: '1', classId: '1' });
     });
   });
 
@@ -87,24 +95,22 @@ describe("test subject router", () => {
     test("delete subject", ()=>{})
   })
 
-
-});
-
-afterAll(() => {
-  sequelize.close();
+	afterAll(() => {
+		sequelize.close();
+	});
 });
 
 const expectedAllSubjects = [
-  { subjectId: 1, name: "Testing SW20" },
-  { subjectId: 2, name: "Testing SW21" },
-  { subjectId: 3, name: "Testing SW22" },
-  { subjectId: 4, name: "Web Development WD20" },
-  { subjectId: 5, name: "Web Development WD21" },
-  { subjectId: 6, name: "Web Development WD22" },
-  { subjectId: 7, name: "Databases SW20" },
-  { subjectId: 8, name: "Databases SW21" },
-  { subjectId: 9, name: "Databases SW22" },
-  { subjectId: 10, name: "Large Systems SW20" },
-  { subjectId: 11, name: "Large Systems SW21" },
-  { subjectId: 12, name: "Large Systems SW22" },
+	{ subjectId: 1, name: "Testing SW20", teacherUserId: 1, classId: 1 },
+  { subjectId: 2, name: "Testing SW21", teacherUserId: 1, classId: 3 },
+  { subjectId: 3, name: "Testing SW22", teacherUserId: 1, classId: 5 },
+  { subjectId: 4, name: "Web Development WD20", teacherUserId: 2, classId: 2 },
+  { subjectId: 5, name: "Web Development WD21", teacherUserId: 2, classId: 4 },
+  { subjectId: 6, name: "Web Development WD22", teacherUserId: 2, classId: 6 },
+  { subjectId: 7, name: "Databases SW20", teacherUserId: 3, classId: 1 },
+  { subjectId: 8, name: "Databases SW21", teacherUserId: 3, classId: 3 },
+  { subjectId: 9, name: "Databases SW22", teacherUserId: 3, classId: 5 },
+  { subjectId: 10, name: "Large Systems SW20", teacherUserId: 4, classId: 1 },
+  { subjectId: 11, name: "Large Systems SW21", teacherUserId: 4, classId: 3 },
+  { subjectId: 12, name: "Large Systems SW22", teacherUserId: 4, classId: 5 },
 ];
