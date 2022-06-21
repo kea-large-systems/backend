ea5e90f5a9022b4c00d53d4345dd10f08b1ff5da
DryDish <57399961+DryDish@users.noreply.github.com>
2022-06-21 14:04:48 +0200

    Circleci editor/669/main (#26)
    * rename classId to class name in subject/by-teacher
    
    * 'fix' tests

--- docs/population-script.sql
+++ docs/population-script.sql
@@ -8,42 +8,42 @@ INSERT INTO roles VALUES(2, "student");
 -- Users ---------------------------------------------------------------------
 
 -- Teachers
-INSERT INTO users VALUES(1,  'Teacher Bob', 'alex320i@stud.kea.dk',           1);
+INSERT INTO users VALUES(1,  'Teacher Bob', 'alex320i@stud.kea.dk', 1);
 INSERT INTO users VALUES(2,  'Teacher Ann', 'ann@kea.dk',           1);
 INSERT INTO users VALUES(3,  'Teacher Won', 'won@kea.dk',           1);
 INSERT INTO users VALUES(4,  'Teacher Tom', 'tom@kea.dk',           1);
 
 -- Students
-INSERT INTO users VALUES(6,  'Student Ada', 'cris2041@stud.kea.dk', 2);
-INSERT INTO users VALUES(7,  'Student Pam', 'pam@stud.kea.dk', 2);
-INSERT INTO users VALUES(8,  'Student Kit', 'kit@stud.kea.dk', 2);
-INSERT INTO users VALUES(9,  'Student Zoe', 'zoe@stud.kea.dk', 2);
-INSERT INTO users VALUES(10,  'Student Ray', 'ray@stud.kea.dk', 2);
-INSERT INTO users VALUES(11, 'Student Alf', 'alf@stud.kea.dk', 2);
-INSERT INTO users VALUES(12, 'Student Coy', 'coy@stud.kea.dk', 2);
-INSERT INTO users VALUES(13, 'Student Gil', 'gil@stud.kea.dk', 2);
+INSERT INTO users VALUES(6,     'Student Ada', 'cris2041@stud.kea.dk',  2);
+INSERT INTO users VALUES(7,     'Student Pam', 'pam@stud.kea.dk',       2);
+INSERT INTO users VALUES(8,     'Student Kit', 'kit@stud.kea.dk',       2);
+INSERT INTO users VALUES(9,     'Student Zoe', 'zoe@stud.kea.dk',       2);
+INSERT INTO users VALUES(10,     'Student Ray', 'ray@stud.kea.dk',      2);
+INSERT INTO users VALUES(11,    'Student Alf', 'alf@stud.kea.dk',       2);
+INSERT INTO users VALUES(12,    'Student Coy', 'coy@stud.kea.dk',       2);
+INSERT INTO users VALUES(13,    'Student Gil', 'gil@stud.kea.dk',       2);
 
 -- Classes ---------------------------------------------------------------------
-INSERT INTO classes VALUES(1, "SW20");
-INSERT INTO classes VALUES(2, "WD20");
-INSERT INTO classes VALUES(3, "SW21");
-INSERT INTO classes VALUES(4, "WD21");
-INSERT INTO classes VALUES(5, "SW22");
-INSERT INTO classes VALUES(6, "WD22");
+INSERT INTO classes VALUES(1,   "SW20");
+INSERT INTO classes VALUES(2,   "WD20");
+INSERT INTO classes VALUES(3,   "SW21");
+INSERT INTO classes VALUES(4,   "WD21");
+INSERT INTO classes VALUES(5,   "SW22");
+INSERT INTO classes VALUES(6,   "WD22");
 
 -- Subjects --------------------------------------------------------------------
-INSERT INTO subjects VALUES(1,  "Testing SW20",         1, 1);
-INSERT INTO subjects VALUES(2,  "Testing SW21",         1, 3);
-INSERT INTO subjects VALUES(3,  "Testing SW22",         1, 5);
-INSERT INTO subjects VALUES(4,  "Web Development WD20", 2, 2);
-INSERT INTO subjects VALUES(5,  "Web Development WD21", 2, 4);
-INSERT INTO subjects VALUES(6,  "Web Development WD22", 2, 6);
-INSERT INTO subjects VALUES(7,  "Databases SW20",       3, 1);
-INSERT INTO subjects VALUES(8,  "Databases SW21",       3, 3);
-INSERT INTO subjects VALUES(9,  "Databases SW22",       3, 5);
-INSERT INTO subjects VALUES(10, "Large Systems SW20",   4, 1);
-INSERT INTO subjects VALUES(11, "Large Systems SW21",   4, 3);
-INSERT INTO subjects VALUES(12, "Large Systems SW22",   4, 5);
+INSERT INTO subjects VALUES(1,  "Testing",           1, 1);
+INSERT INTO subjects VALUES(2,  "Testing",           1, 3);
+INSERT INTO subjects VALUES(3,  "Testing",           1, 5);
+INSERT INTO subjects VALUES(4,  "Web Development",   2, 2);
+INSERT INTO subjects VALUES(5,  "Web Development",   2, 4);
+INSERT INTO subjects VALUES(6,  "Web Development",   2, 6);
+INSERT INTO subjects VALUES(7,  "Databases",         3, 1);
+INSERT INTO subjects VALUES(8,  "Databases",         3, 3);
+INSERT INTO subjects VALUES(9,  "Databases",         3, 5);
+INSERT INTO subjects VALUES(10, "Large Systems",     4, 1);
+INSERT INTO subjects VALUES(11, "Large Systems",     4, 3);
+INSERT INTO subjects VALUES(12, "Large Systems",     4, 5);
 
 -- Lectures --------------------------------------------------------------------
 INSERT INTO lectures VALUES(1, "Not Learning Microservices 1", '2022-06-06 13:14',  ('2022-06-06 13:14' + interval  90 minute), 12);
--- test/routes/subject-router.test.ts
+++ test/routes/subject-router.test.ts
@@ -44,7 +44,7 @@ describe("test subject router", () => {
       const response = await request(app)
         .get("/1");
       expect(response.status).toBe(200);
-      expect(response.body).toStrictEqual({ name: "Testing SW20", subjectId: 1, classId: 1, teacherUserId: 1 });
+      expect(response.body).toStrictEqual({ name: "Testing", subjectId: 1, classId: 1, teacherUserId: 1 });
     });
     test("ID that does not exist in the database", async () => {
       const response = await request(app)
@@ -124,9 +124,9 @@ describe("test subject router", () => {
       const response =await request(app)
         .get("/by-teacher/1")
       expect(response.body).toStrictEqual([
-        { subjectId: 1, name: 'Testing SW20', teacherUserId: 1, classId: 1 },
-        { subjectId: 2, name: 'Testing SW21', teacherUserId: 1, classId: 3 },
-        { subjectId: 3, name: 'Testing SW22', teacherUserId: 1, classId: 5 }
+        { subjectId: 1, name: 'Testing', teacherUserId: 1, classId: "SW20" },
+        { subjectId: 2, name: 'Testing', teacherUserId: 1, classId: "SW21" },
+        { subjectId: 3, name: 'Testing', teacherUserId: 1, classId: "SW22" }
       ])
     })
     test("test to get teacher classes for non exist teacher", async ()=> {
@@ -142,16 +142,16 @@ describe("test subject router", () => {
 });
 
 const expectedAllSubjects = [
-  { subjectId: 1, name: "Testing SW20", teacherUserId: 1, classId: 1 },
-  { subjectId: 2, name: "Testing SW21", teacherUserId: 1, classId: 3 },
-  { subjectId: 3, name: "Testing SW22", teacherUserId: 1, classId: 5 },
-  { subjectId: 4, name: "Web Development WD20", teacherUserId: 2, classId: 2 },
-  { subjectId: 5, name: "Web Development WD21", teacherUserId: 2, classId: 4 },
-  { subjectId: 6, name: "Web Development WD22", teacherUserId: 2, classId: 6 },
-  { subjectId: 7, name: "Databases SW20", teacherUserId: 3, classId: 1 },
-  { subjectId: 8, name: "Databases SW21", teacherUserId: 3, classId: 3 },
-  { subjectId: 9, name: "Databases SW22", teacherUserId: 3, classId: 5 },
-  { subjectId: 10, name: "Large Systems SW20", teacherUserId: 4, classId: 1 },
-  { subjectId: 11, name: "Large Systems SW21", teacherUserId: 4, classId: 3 },
-  { subjectId: 12, name: "Large Systems SW22", teacherUserId: 4, classId: 5 },
+  { subjectId: 1, name: "Testing", teacherUserId: 1, classId: 1 },
+  { subjectId: 2, name: "Testing", teacherUserId: 1, classId: 3 },
+  { subjectId: 3, name: "Testing", teacherUserId: 1, classId: 5 },
+  { subjectId: 4, name: "Web Development", teacherUserId: 2, classId: 2 },
+  { subjectId: 5, name: "Web Development", teacherUserId: 2, classId: 4 },
+  { subjectId: 6, name: "Web Development", teacherUserId: 2, classId: 6 },
+  { subjectId: 7, name: "Databases", teacherUserId: 3, classId: 1 },
+  { subjectId: 8, name: "Databases", teacherUserId: 3, classId: 3 },
+  { subjectId: 9, name: "Databases", teacherUserId: 3, classId: 5 },
+  { subjectId: 10, name: "Large Systems", teacherUserId: 4, classId: 1 },
+  { subjectId: 11, name: "Large Systems", teacherUserId: 4, classId: 3 },
+  { subjectId: 12, name: "Large Systems", teacherUserId: 4, classId: 5 },
 ];
